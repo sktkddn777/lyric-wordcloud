029530d7f886105be1da93676ffd9f4a11516456
sktkddn777 <sktkddn777@naver.com>
2021-10-13 13:29:43 +0900

    distinguish eng and kor song

--- cloud/make_cloud.py
+++ cloud/make_cloud.py
@@ -5,38 +5,46 @@ import matplotlib.pyplot as plt
 import jpype
 from konlpy.tag import Okt 
 
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
 
 def tokenizer_konlpy(text):
   okt = Okt()
   return [word for word in okt.nouns(text) if len(word) >1]
 
 
-def wordCloud(lyric_lst):
-  stopwords = []
-  with open('cloud/stopwords.txt', 'r', encoding='UTF8') as file:
-    for text in file:
-      stopwords.append(text.strip('\n'))
-  
+def wordCloud(lyric_lst, language):
+  if language == 'en':
+    stopword = stopwords.words('english')
+  else:
+    stopword = []
+    with open('cloud/stopwords.txt', 'r', encoding='UTF8') as file:
+      for text in file:
+        stopword.append(text.strip('\n'))
+
   # 불용어에 없는 단어들만 추출.
   normalized_lyric = []
   for lyric in lyric_lst:
-    if lyric not in stopwords:
+    if lyric not in stopword:
       normalized_lyric.append(lyric)
 
   normalized_lyric = " ".join(normalized_lyric)
 
-  # 명사만 뽑음.
-  noun = tokenizer_konlpy(normalized_lyric)
-  
-  count = Counter(noun)
-  noun_list = count.most_common(100)
+  # tokenize
+  if language == 'ko':
+    word = tokenizer_konlpy(normalized_lyric)
+  else:
+    word = word_tokenize(normalized_lyric)
+
+  count = Counter(word)
+  word_list = count.most_common(100)
 
   wordcloud = WordCloud(font_path='cloud/font/NanumGothic-Regular.ttf', 
                         background_color = 'white',
                         width=512, height=512,
                         max_font_size=500,
                         max_words=100)
-  wordcloud.generate_from_frequencies(dict(noun_list))
+  wordcloud.generate_from_frequencies(dict(word_list))
 
 
   plt.figure(figsize=(22,22)) #이미지 사이즈 지정
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from api.vibe_api import Vibe
 from cloud.make_cloud import wordCloud
+import re
 
 def find_id():
-  
   singer_name = input("가수 이름: ")
   song = input("노래 이름: ")
+
   vibe = Vibe()
   lyric = vibe.lyrics(vibe.find(singer_name, song))
-  return lyric
+
+  if singer_name.encode().isalpha() and song.encode().isalpha():
+    return lyric, 'en'
+  return lyric, 'ko'
+
 
 if __name__ == "__main__":
-  lyric = find_id()
+  lyric, language = find_id()
+
   if lyric:
-    wordCloud(lyric)
-  
+    wordCloud(lyric, language)
+      
+
